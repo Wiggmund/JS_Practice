426719b01a3464f67fdb6c8aec1a420f33cc6f1c
Wiggmund <djsemua2000@gmail.com>
2018-08-28 18:58:38 +0300

    Area for testing some features and just for repeating

---
+++ for_practice.js
@@ -0,0 +1,12 @@
+setTimeout(function ([{twoNum:{a:ten = -10, b:onehundred = -100}}]) {
+    console.log(`
+        \n Ten: ${ten}
+        \n Onehundred: ${onehundred}
+    `);
+}, 3000, [
+    {
+    twoNum: {
+        a: undefined, 
+        b: undefined
+    }
+    }]);
